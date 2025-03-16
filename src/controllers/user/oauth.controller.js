5e5e564ca30dce9c6f80a971ad9f0de289f6907b
DonggunLim <Ldonggun6766@gmail.com>
2025-03-16 16:32:55 +0900

    fix: loginStatus cookie 설정 수정

--- src/controllers/user/oauth.controller.js
+++ src/controllers/user/oauth.controller.js
@@ -70,6 +70,9 @@ class OauthController {
         res.cookie('loginStatus', 'true', {
           httpOnly: false,
           maxAge: 60 * 60 * 1000,
+          secure: true,
+          sameSite: 'None',
+          domain: 'https://petple-front.vercel.app/',
           path: '/', //모든 경로에 쿠키포함
         });
 
