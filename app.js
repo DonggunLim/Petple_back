4054faa8c7f138182afa2f2393cc73487f84b734
DonggunLim <Ldonggun6766@gmail.com>
2025-03-16 19:00:42 +0900

    fix: 오타수정

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use();
+app.use(cors());
 
 //라우터
 app.use('/api/my', userRoutes);
